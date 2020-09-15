a5975c5df56561a64dd3a44acf60e2fd9e7a82b9
Simon <orlhatundji@gmail.com>
2020-09-15 15:02:28 +0100

    added celciu-fahrenheit converter

---
+++ temperatureconverter.py
@@ -0,0 +1,9 @@
+def celciusToFahrenheit(val):
+    return (val * 9/5) + 32
+
+def fahrenheitToCelsius(val):
+    return (val - 32) * 5/9
+
+
+print(celciusToFahrenheit(0))
+print(fahrenheitToCelsius(32))
